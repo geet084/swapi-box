eaf7a95b82e553d964e075c9840b72a797b45d6c
Travis Gee <geet084@gmail.com>
2019-01-23 08:36:56 -0700

    Add ability to display random movie opening crawl

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
 import './App.scss';
+import ScrollBox from '../ScrollBox/scrollBox'
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: []
+    }
+  }
+
+  componentDidMount = () => {
+    fetch('https://swapi.co/api/films/')
+      .then(data => data.json())
+      .then(data => this.setState({ movies: data.results }))
+  }
+
+  showRandomMovie = () => {
+    const { movies } = this.state;
+    let maxNum = Math.floor(movies.length);
+    let randomNumber = Math.floor(Math.random() * maxNum)
+
+    return movies[randomNumber].opening_crawl;
+  }
+
   render() {
-    return (
-      <div className="App">
-        SWAPI BOX
-      </div>
-    );
+    
+    if (this.state.movies.length > 0) {
+      return (
+        <div className="App">
+          <ScrollBox movie={this.showRandomMovie()}/>
+          <h2>SWAPI BOX</h2>
+        </div>
+      );
+    } else {
+      return <div>nothing to show</div>
+    }
   }
 }
