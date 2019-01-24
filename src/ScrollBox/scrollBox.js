9dece2daf4554a200dcf374e9c837429c4fbe2a2
Travis Gee <geet084@gmail.com>
2019-01-24 11:25:48 -0700

    Add scroll styling

--- src/ScrollBox/scrollBox.js
+++ src/ScrollBox/scrollBox.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import './ScrollBox.scss'
 
 const scrollBox = ({ movie }) => {
-  return <div>{movie}</div>
+  return (
+    <marquee
+      className="scroll"
+      behavior="scroll"
+      direction="up"
+      scrollamount="1.5">
+      {movie}
+    </marquee>
+  )
 }
 
 export default scrollBox;
