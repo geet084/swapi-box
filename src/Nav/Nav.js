ebb8eb4f35bb05eb2b23c8f6a2a67153476876db
Travis Gee <geet084@gmail.com>
2019-01-24 11:26:17 -0700

    Add nav styling & buttons

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import './Nav.scss'
 
 const Nav = (props) => {
   // console.log(props)
   return (
-    <div>
-      <button onClick={props.showMore}>PEOPLE</button>
+    <div className="nav">
+      <button
+        onClick={props.showPeople}>
+        PEOPLE
+      </button>
+      <button
+        onClick={props.showPlanets}>
+        PLANETS
+      </button>
+      <button
+        onClick={props.showVehicles}>
+        VEHICLES
+      </button>
     </div>
   )
 }
