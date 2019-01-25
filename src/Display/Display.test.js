b810494363daa349151cf52b77504817ea5e52c8
Travis Gee <geet084@gmail.com>
2019-01-25 08:12:39 -0700

    Add tests to display

---
+++ src/Display/Display.test.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Display from './Display'
+
+describe('Display', () => {
+  let wrapper;
+
+  it('should properly render default snapshot', () => {
+    const mockChoice = ''
+    const mockThing = { choice: mockChoice, vehicles: [] }
+    wrapper = shallow(
+      <Display
+        choice={mockChoice}
+        {...mockThing} />
+    )
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should properly render a chosen category', () => {
+    const mockChoice = 'vehicles'
+    const mockThing = { choice: mockChoice, vehicles: [] }
+    wrapper = shallow(
+      <Display
+        choice={mockChoice}
+        {...mockThing} />
+    )
+
+    expect(wrapper).toMatchSnapshot();
+  })
+})
