b40b44617b20e43761761e46f70593babbee08db
Travis Gee <geet084@gmail.com>
2019-01-25 08:12:58 -0700

    Add tests to card

---
+++ src/Card/Card.test.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Card from './Card'
+
+describe('Card', () => {
+  let wrapper;
+
+  it('should properly render snapshot', () => {
+    const mockChoice = 'people';
+    const mockSelection = {name: 'hello'};
+    wrapper = shallow(
+      <Card choice={mockChoice} {...mockSelection} />
+    )
+    wrapper.setProps({choice: 'people'})
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should properly render snapshot', () => {
+    const mockChoice = 'planets';
+    const mockSelection = { name: 'hello' };
+    wrapper = shallow(
+      <Card choice={mockChoice} {...mockSelection} />
+    )
+    wrapper.setProps({ choice: 'planets' })
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should properly render snapshot', () => {
+    const mockChoice = 'vehicles';
+    const mockSelection = { name: 'hello' };
+    wrapper = shallow(
+      <Card choice={mockChoice} {...mockSelection} />
+    )
+    wrapper.setProps({ choice: 'vehicles' })
+    expect(wrapper).toMatchSnapshot()
+  })
+
+})
