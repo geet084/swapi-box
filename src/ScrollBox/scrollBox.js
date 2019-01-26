82e192da2b5cf6352aa416ad85c5d5cce3fe5133
Travis Gee <geet084@gmail.com>
2019-01-26 10:09:23 -0700

    Update crawl to include title/year

--- src/ScrollBox/scrollBox.js
+++ src/ScrollBox/scrollBox.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import './ScrollBox.scss'
 
-const scrollBox = ({ crawlText }) => {
+const scrollBox = ({ crawlText, title, date }) => {
+  const scroll = (
+    <div>
+      <p>{crawlText}</p>
+      <p className="title"><span>{title}  &nbsp; &nbsp; {date}</span></p> 
+    </div>
+  )
   return (
     <marquee
       className="scroll"
       behavior="scroll"
       direction="up"
       scrollamount="1.5">
-      {crawlText}
+      {scroll}
     </marquee>
   )
 }
