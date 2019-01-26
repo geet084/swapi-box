9616debfc663e324f968a01e0b9fad7bc792a234
Travis Gee <geet084@gmail.com>
2019-01-26 16:37:15 -0700

    Update properties based on fetch call

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -2,17 +2,16 @@ import React from 'react'
 import './Card.scss'
 
 const Card = (props) => {
-
   const classes = `card ${props.choice}-bg`
   
-  if (props.choice === 'people') {
+ if (props.choice === 'people') {
     return (
       <div className={classes}>
         <span className="fave-icon">fave</span>
         <p>Name: {props.name}</p>
-        <p>HomeWorld: {props.world}</p>
+        <p>HomeWorld: {props.homeworld}</p>
         <p>Population: {props.population}</p>
-        <p>Species: {props.spec}</p>
+        <p>Species: {props.species}</p>
       </div>
     )
   } else if (props.choice === 'planets') {
@@ -23,7 +22,7 @@ const Card = (props) => {
         <p>Terrain: {props.terrain}</p>
         <p>Population: {props.population}</p>
         <p>Climate: {props.climate}</p>
-        <p>Residents: {props.inhabitants}</p>
+        <p>Residents: {props.residents}</p>
       </div>
     )
   } else {
