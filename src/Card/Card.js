fd639890002d01af1c3d74a374d3f657fe23fbcb
Travis Gee <geet084@gmail.com>
2019-01-27 11:58:51 -0700

    Refactor to be more dynamic

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -4,52 +4,27 @@ import PropTypes from 'prop-types'
 
 const Card = (props) => {
   const classes = `card ${props.choice}-bg`
-  
- if (props.choice === 'people') {
-    return (
-      <div className={classes}>
-        <span className="fave-icon">
-          <i className="fas fa-jedi"></i>
-        </span>
-        <p>Name: {props.name}</p>
-        <p>HomeWorld: {props.homeworld}</p>
-        <p>Population: {props.population}</p>
-        <p>Species: {props.species}</p>
-      </div>
-    )
- } else if (props.choice === 'planets') {
-   console.log(props)
-   let residents = props.residents.map((resident, i) => {
-     if (i === 0) return `${resident}`
-     else return `,  ${resident}`
-   })
 
-    return (
-      <div className={classes}>
-        <span className="fave-icon">
-          <i className="fas fa-jedi"></i>
-        </span>
-        <p>Name: {props.name}</p>
-        <p>Terrain: {props.terrain}</p>
-        <p>Population: {props.population}</p>
-        <p>Climate: {props.climate}</p>
-        <p>Residents: {residents}</p>
-      </div>
-    )
-  } else {
-    return (
-      <div className={classes}>
-        <span className="fave-icon">
-          <i className="fas fa-jedi"></i>
-        </span>
-        <p>Name: {props.name}</p>
-        <p>Model: {props.model}</p>
-        <p>Class: {props.vehicle_class}</p>
-        <p>Passengers: {props.passengers}</p>
-      </div>
-    )
-  }
 
+  let content = Object.keys(props).map(item => {
+    if (item === 'residents') {
+      return item = props.residents.map((resident, i) => {
+        if (i === 0) return `residents: ${resident}`
+        else return `,  ${resident}`
+      })
+    } else if (item !== 'choice') {
+      return <p key={item}>{item}: {props[item]}</p>
+    } else return ''
+  })
+
+  return (
+    <div className={classes}>
+      <span className="fave-icon">
+        <i className="fas fa-jedi"></i>
+      </span>
+      {content}
+    </div>
+  )
 }
 
 Card.propTypes = {
