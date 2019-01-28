a95f39957e33bc0408e3552bd103f4cdcff430b0
Travis Gee <geet084@gmail.com>
2019-01-28 07:15:14 -0700

    Updates

---
+++ precache-manifest.c400b0203f57d77dfa5cb9128395fd45.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae23dfc6d2d9e3c18840d0597d88f1a7",
+    "url": "/swapi-box/./static/media/saber.ae23dfc6.jpg"
+  },
+  {
+    "revision": "f33074395073239e30cc",
+    "url": "/swapi-box/./static/css/main.bcf9794d.chunk.css"
+  },
+  {
+    "revision": "c6c87e91182e6e6b61ae",
+    "url": "/swapi-box/./static/js/1.c6c87e91.chunk.js"
+  },
+  {
+    "revision": "a0e7002f655967bfb033",
+    "url": "/swapi-box/./static/js/runtime~main.a0e7002f.js"
+  },
+  {
+    "revision": "d7a61190a836bdcfc62bf97af4f4c74b",
+    "url": "/swapi-box/./static/media/sw-bg.d7a61190.png"
+  },
+  {
+    "revision": "f33074395073239e30cc",
+    "url": "/swapi-box/./static/js/main.f3307439.chunk.js"
+  },
+  {
+    "revision": "a3aba632127401e0acf3c8271eb75f14",
+    "url": "/swapi-box/./static/media/people-bg.a3aba632.jpg"
+  },
+  {
+    "revision": "c9a7286914fba5cefe55ad57a73ea8e0",
+    "url": "/swapi-box/./static/media/planet-bg.c9a72869.jpg"
+  },
+  {
+    "revision": "a165712d112163c2bb7b6c6955e398fd",
+    "url": "/swapi-box/./static/media/vehicle-bg.a165712d.jpg"
+  },
+  {
+    "revision": "e56061de505205dac3b8f5cd984ee7ea",
+    "url": "/swapi-box/./static/media/splash.e56061de.jpg"
+  },
+  {
+    "revision": "e645f1b2ae8c7cc258c2d7f3fe2f3f1d",
+    "url": "/swapi-box/./index.html"
+  }
+];
